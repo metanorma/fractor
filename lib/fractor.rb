839da5e1dd445133490e5ea30f216df8d35d4ecb
Ronald Tse <ronald.tse@ribose.com>
2025-05-03 16:52:16 +0800

    feat: initial commit

---
+++ lib/fractor.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fractor/version"
+
+module Fractor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/fractor/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Fractor
+  VERSION = "0.1.0"
+end
